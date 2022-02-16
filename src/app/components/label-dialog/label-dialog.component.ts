36d2c683ae9b1abe6ade349b14265af5e0785ffb
Sairam-Reddy <sairamreddyg33@gmail.com>
2022-02-16 21:25:09 +0530

    1 file modified

--- src/app/components/label-dialog/label-dialog.component.ts
+++ src/app/components/label-dialog/label-dialog.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Inject, EventEmitter } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { labelDialog } from './models/label.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-label-dialog',
