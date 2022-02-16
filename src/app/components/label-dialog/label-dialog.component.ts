8d879e1717e381cf2f7a279fadb1940b1a7769c3
Sairam-Reddy <sairamreddyg33@gmail.com>
2022-02-16 21:20:28 +0530

    1 file modified

--- src/app/components/label-dialog/label-dialog.component.ts
+++ src/app/components/label-dialog/label-dialog.component.ts
@@ -8,16 +8,27 @@ import { labelDialog } from './models/label.model';
   styleUrls: ['./label-dialog.component.scss'],
 })
 export class LabelDialogComponent implements OnInit {
+  public formGroup: FormGroup;
+
   constructor(
     private dialogRef: MatDialogRef<LabelDialogComponent>,
     @Inject(MAT_DIALOG_DATA)
     public data: {
       title: string;
       labels: Array<labelDialog>;
-    }
+    },
+    private formBuilder: FormBuilder
   ) {}
 
-  public ngOnInit(): void {}
+  public ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      labels: this.formBuilder.array(
+        this.data.labels.map((label: labelDialog) =>
+          this.createLabelGroup(label)
+        )
+      ),
+    });
+  }
 
   public cancel(): void {
     this.dialogRef.close();
@@ -25,5 +36,14 @@ export class LabelDialogComponent implements OnInit {
 
   public ok(): void {
     this.dialogRef.close();
+  }
+
+  private createLabelGroup(label: labelDialog): FormGroup {
+    return this.formBuilder.group({
+      ...label,
+      name: [label.name, Validators.required],
+      id: [label.id],
+      selected: [label.selected],
+    });
   }
 }
